0f950ff1ddeadca0be18634f4dbe34e1c7a4c8f9
KANAMU-0210 <sskoumi@icloud.com>
2025-12-04 16:28:29 +0900

    start
    update

---
+++ Assets/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public GameObject explosionPrefab;   //爆発エフェクトのPrefab
+
+	void Update () {
+		transform.Translate (0, 0.2f, 0);
+
+		if (transform.position.y > 5) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		// 爆発エフェクトを生成する	
+		GameObject effect = Instantiate (explosionPrefab, transform.position, Quaternion.identity) as GameObject;
+		Destroy (coll.gameObject);	
+		Destroy (gameObject);
+		Destroy (effect, 1.0f);
+	}
+}
